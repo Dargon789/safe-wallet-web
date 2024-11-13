c608bd43ff64a169b1dec0f63bd1e98b6a03be39
Legion's <64915515+Dargon789@users.noreply.github.com>
2024-11-13 09:54:21 +0700

    Update src/hooks/safe-apps/useSafeAppUrl.ts
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    Signed-off-by: Legion's  <64915515+Dargon789@users.noreply.github.com>

--- src/hooks/safe-apps/useSafeAppUrl.ts
+++ src/hooks/safe-apps/useSafeAppUrl.ts
@@ -24,7 +24,7 @@ const useSafeAppUrl = (): string | undefined => {
     }
   }, [router])
 
-  return useMemo(() => (appUrl ? sanitizeUrl(appUrl) : undefined), [appUrl])
+  return useMemo(() => appUrl, [appUrl])
 }
 
 export { useSafeAppUrl }
