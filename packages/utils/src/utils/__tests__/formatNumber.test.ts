74a6c1af91de220468091c507924f229d9ed06e6
AU_gdev_19 <64915515+Dargon789@users.noreply.github.com>
2025-05-15 14:31:28 +0700

    yarn config bug

--- packages/utils/src/utils/__tests__/formatNumber.test.ts
+++ packages/utils/src/utils/__tests__/formatNumber.test.ts
@@ -1,4 +1,9 @@
-import { formatAmountPrecise, formatAmount, formatCurrency, formatCurrencyPrecise } from '@safe-global/utils/utils/formatNumber'
+import {
+  formatAmountPrecise,
+  formatAmount,
+  formatCurrency,
+  formatCurrencyPrecise,
+} from '@safe-global/utils/utils/formatNumber'
 
 describe('formatNumber', () => {
   describe('formatAmountPrecise', () => {
